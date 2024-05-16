0bbbeb6337cf0a808b7d26a87802c9d53469eee5
Eyzou <elodie.hamm@gmail.com>
2024-05-16 18:55:58 +0200

    adding gnl

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ehamm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 13:57:29 by ehamm             #+#    #+#             */
-/*   Updated: 2024/02/26 10:50:03 by ehamm            ###   ########.fr       */
+/*   Updated: 2024/04/17 11:38:14 by ehamm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 		return ;
 	tmp = *lst;
 	while (tmp)
-	{	
+	{
 		next = tmp -> next;
 		del(tmp -> content);
 		free(tmp);
